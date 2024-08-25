cda8eef9e49dcfa2cc280d9561c05692696366e7
Ajeet_rana <r72139624@gmail.com>
2024-08-25 23:53:55 +0530

    Updated 3

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { supabaseKey } from "../../config";
 export const supabaseUrl = "https://slmqauetfcqbzhxdgeir.supabase.co";
-//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsbXFhdWV0ZmNxYnpoeGRnZWlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAxMDgxNTQsImV4cCI6MjAzNTY4NDE1NH0.cGzE4lhfk7olfthl1MinF_4QUBFFggLNAKxDg8p0XlQ";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
